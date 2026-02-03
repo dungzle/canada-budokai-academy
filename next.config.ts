c75e50509687ca9435e2dc0c3d14f46c56441ff5
Le <vietdung.le@cgi.com>
2026-02-03 09:37:11 -0800

    updated next config and rebuild

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/canada-budokai-academy",
+  assetPrefix: "/canada-budokai-academy",
   images: {
     unoptimized: true,
   },
-  basePath: "",
 };
 
 module.exports = nextConfig;
